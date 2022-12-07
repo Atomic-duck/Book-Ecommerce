bd203dc32eea7d680e9f569c6347fc04eac8f69f
thanhngodinh <thanh.ngodinh2000@gmail.com>
2022-12-07 20:06:43 +0700

    Add Admin

---
+++ admin/views/edit_logo_view.php
@@ -0,0 +1,30 @@
+<?php
+if (isset($_GET['status'])) {
+    if ($_GET['status'] == 'edit') {
+        $id = $_GET['id'];
+        $logo = $obj->display_logo_ID($id);
+    }
+}
+if (isset($_POST['update_logo'])) {
+    $up_msg = $obj->update_logo($_POST);
+}
+?>
+
+
+<h2>Update Logo</h2>
+
+<h6 class="">
+    <?php if (isset($up_msg)) {
+        echo $up_msg;
+    } ?>
+</h6>
+
+<form action="" enctype="multipart/form-data" method="post">
+    <div class="form-group">
+        <label for="img">Image</label>
+        <input type="file" name="img" class="form-control">
+        <input type="hidden" name="id" value="<?php echo $logo['id'] ?>">
+        <br />
+        <input type="submit" value="Update Logo" name="update_logo" class="btn btn-primary">
+    </div>
+</form>
